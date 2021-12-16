f76f858c9e0a80046d941eefb27491ffce622879
ecbaum <carlbaum.erik@gmail.com>
2021-12-16 10:12:34 +0000

    Color extractor finished. RGB value -> Color name remains

--- scripts/color_extractor_node.py
+++ scripts/color_extractor_node.py
@@ -5,37 +5,45 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import cv2
-
+from robotic_pusher.srv import *
 
 class color_extractor:
 
-  def __init__(self):
-    self.pub = rospy.Publisher("extracted_color", String, queue_size=10)
-    self.sub = rospy.Subscriber("/xtion/rgb/image_color", Image, self.callback)
-    self.bridge = CvBridge()
+    def __init__(self):
 
-  def callback(self, img_msg):
-    try:
-        cv2_img = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
-    except CvBridgeError as e:
-        print(e)
+        self.subscriber = rospy.Subscriber("/xtion/rgb/image_color", Image, self.callback)
+        self.service = rospy.Service('/robotic_pusher/getColor', getColor, self.returnColor)
 
+        self.bridge = CvBridge()
+        self.color_value = []
 
-    if rospy.has_param('/color_extraction_pixel_location/x'):
-        x = rospy.get_param('/color_extraction_pixel_location/x')
-    else:
-        x = 200 # default value
-    if rospy.has_param('/color_extraction_pixel_location/y'):
-        y = rospy.get_param('/color_extraction_pixel_location/y')
-    else:
-        y = 200 # default value
+        self.x = 200 # default value
+        self.y = 200 # default value
+
+        if rospy.has_param('/color_extraction_pixel_location/x'): #If params are loaded, read them
+            self.x = rospy.get_param('/color_extraction_pixel_location/x')
+        if rospy.has_param('/color_extraction_pixel_location/y'):
+            self.y = rospy.get_param('/color_extraction_pixel_location/y')
+        
     
+    def callback(self, img_msg):
+        try:
+            cv2_img = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
+        except CvBridgeError as e:
+            print(e)
 
-    rgb = cv2_img[x, y, 0:3]
+        rgb = cv2_img[self.x, self.y, 0:3]
+        self.color_value = rgb
 
-    rospy.loginfo("rgb: " + str(rgb))
+        rospy.loginfo("Pixel: [" + str(self.x) + ", " + str(self.y) + "], Val: "  + str(rgb) )
     
-    self.pub.publish(str(rgb))
+    def decodeRGB(self):
+        color = str(self.color_value) # TODO: implement dictionary between rgb values and color names
+        return color
+
+    def returnColor(self, req):
+        return getColorResponse(self.decodeRGB())
+
 
 def main(args):
     extractor = color_extractor()
