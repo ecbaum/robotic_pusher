021dc24f5867ef9cd9377fbcc571a06e41d62aa2
masplund91 <masplund91@gmail.com>
2021-12-18 16:13:45 +0000

    Action node is done

--- src/get_action_node.cpp
+++ src/get_action_node.cpp
@@ -21,30 +21,30 @@
 #include <fstream>
 #include <iostream>
 #include <pthread.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 /*********************************************************************
  * ROS INCLUDES
  ********************************************************************/
 #include <ros/ros.h>
 #include <std_msgs/String.h>
-// #include <tf/transform_broadcaster.h>
-// #include <tf/transform_listener.h>
-// #include <tf_conversions/tf_eigen.h>
-// #include <visualization_msgs/Marker.h>
-// #include <visualization_msgs/MarkerArray.h>
-
 /*********************************************************************
  * CUSTOM INCLUDES
  * ******************************************************************/
-// #include <the_drink_pack/DrinkClass.h> // Might want to have a class
+#include <getopt.h>
 #include <map>
 #include <robotic_pusher/getVelocity.h>
 #include <robotic_pusher/getWeightType.h>
 #include <robotic_pusher/spawnObject.h>
+#include <rosprolog/rosprolog_client/PrologClient.h>
+#include <signal.h>
+#include <vector>
 
 using namespace std;
 
 #define object_name "random"
+#define file_name "ont_file.txt"
+string ontology_name = "cube";
 #define desired_distance 10 // cm??
 
 /*  Weight ids  */
@@ -54,66 +54,165 @@ std::map<std::string, int> weight_id{
     {"heavy", 2},
 };
 
-/*  Global parameters for the clients   */
-ros::ServiceClient client_spawn;
-ros::ServiceClient client_weight;
+/*  Functions   */
+float get_action(string weight, PrologClient pl) {
+  /*    Check for heavy/medium/light class  */
 
-robotic_pusher::spawnObject object;
-robotic_pusher::getWeightType srv;
+  std::vector<float> distance_vector;
+  std::vector<float> velocity_vector;
 
-float get_action(int weight) {
-  switch (weight) {
-  case 0:
-    return 0.2f;
-    break;
-  case 1:
-    return 0.5f;
-    break;
-  case 2:
-    return 0.7f;
-    break;
-  default:
-    ROS_INFO_STREAM("Unknown weight id...");
-    break;
+  PrologQuery bdgs =
+      pl.query("owl_individual_of(I," + ontology_name + ":" + weight + ")");
+
+  for (PrologQuery::iterator it = bdgs.begin(); it != bdgs.end(); it++) {
+    PrologBindings bdg = *it;
+    string instance = bdg["I"].toString();
+    /*  Extract distance    */
+    PrologQuery classbdgs =
+        pl.query("rdf(" + ontology_name + ":'" + instance + "', distance, X)");
+    PrologQuery::iterator iterator = classbdgs.begin();
+    bdg = *iterator;
+    string distance = bdg["X"].toString();
+    distance.erase(0, 1);
+    distance.erase(distance.size() - 1);
+    float distance_traveled = std::stof(distance);
+    distance_vector.push_back(distance_traveled);
+    /*  Extract velocity    */
+    classbdgs =
+        pl.query("rdf(" + ontology_name + ":'" + instance + "', velocity, X)");
+    iterator = classbdgs.begin();
+    bdg = *iterator;
+    string velocity = bdg["X"].toString();
+    velocity.erase(0, 1);
+    velocity.erase(velocity.size() - 1);
+    float used_velocity = std::stof(velocity);
+    velocity_vector.push_back(used_velocity);
   }
-  return 0.f;
-  // Temporary, random between 0.0 and 1.0
-  // return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+
+  /*  Find the closest points in distance     */
+  float min_upper = FLT_MAX, min_lower = FLT_MAX;
+  int lower_bound, upper_bound, index = 0;
+
+  for (float d : distance_vector) {
+    float diff = abs(d - desired_distance);
+
+    if (diff < min_upper && d > desired_distance) {
+      min_upper = diff;
+      upper_bound = index;
+    } else if (diff < min_lower && d < desired_distance) {
+      min_lower = diff;
+      lower_bound = index;
+    }
+
+    index++;
+  }
+
+  /*    Interpolate the velocity    */
+  float a = velocity_vector[lower_bound];
+  float b = velocity_vector[upper_bound];
+  float t = desired_distance / distance_vector[upper_bound];
+
+  return a + t * (b - a);
 }
 
-bool get_velocity(robotic_pusher::getVelocity::Request &req,
-                  robotic_pusher::getVelocity::Response &res) {
+void load_ontology(PrologClient pl) {
+  string line;
 
-  // Pretty dumb check but why not
-  if (!req.get_velocity) {
-    return false;
+  ifstream input_file(file_name);
+  if (!input_file.is_open()) {
+    cerr << "Could not open the file - '" << file_name
+         << "' Using ontology without instance" << endl;
+    return;
   }
 
-  /*  Call the service to spawn a object  */
-  if (client_spawn.call(object)) {
-    ROS_INFO_STREAM("Response: " << object.response.reply);
+  while (getline(input_file, line)) {
+    pl.query(line);
+  }
+
+  input_file.close();
+}
+
+string find_instance_name(string instance_name, string Class, PrologClient pl,
+                          int i = 0) {
+
+  PrologQuery bdgs =
+      pl.query("owl_individual_of(" + ontology_name + ":'" + instance_name +
+               "'," + ontology_name + ":'" + Class + "')");
+  bool res = false;
+  for (auto &it : bdgs) {
+    res = true;
+    break;
+  }
+  if (res) {
+    string instance_name_new = instance_name + to_string(i);
+    find_instance_name(instance_name_new, Class, pl, i++);
   } else {
-    ROS_ERROR_STREAM("Failed to spawn a random object");
-    return false;
+    return instance_name;
   }
+}
 
-  int object_weight_id;
-  /*  Call the service to get the weight of the spawned object  */
-  if (client_weight.call(srv)) {
-    ROS_INFO_STREAM("Response: " << srv.response.weight_type);
-    object_weight_id = weight_id[srv.response.weight_type];
+void update_onotology(std::ofstream &file, float distance, float velocity,
+                      string object, PrologClient pl) {
+  /*    Check if class exist    */
+  PrologQuery bdgs = pl.query("rdf_has(" + ontology_name + ":'" + object +
+                              "', rdf:type, owl:'Class')");
+  bool res = false;
+  for (auto &it : bdgs) {
+    res = true;
+    break;
+  }
+  if (res) {
+    string instance_name = find_instance_name(object, object, pl);
+    string query = "rdf_assert(" + ontology_name + ":'" + instance_name +
+                   "', rdf:type, " + ontology_name + ":'" + object + "')";
+    /*  Create instance  */
+    pl.query(query);
+    file << query + "\n";
+    /*  Create data propery distance  */
+    query = "rdf_assert(" + ontology_name + ":'" + instance_name +
+            "', velocity , '" + to_string(velocity) + "')";
+    pl.query(query);
+    file << query + "\n";
+    /*  Create data propery velocity  */
+    query = "rdf_assert(" + ontology_name + ":'" + instance_name +
+            "', distance , '" + to_string(distance) + "')";
+    pl.query(query);
+    file << query + "\n";
+
   } else {
-    ROS_ERROR_STREAM("Failed to get the weight from the object");
-    return false;
+    ROS_INFO_STREAM("Class " << object << " does not exist in the ontology");
   }
+}
 
-  // Change this function to get different actions
-  res.impact_velocity = get_action(object_weight_id);
-
-  return true;
+void my_handler(int s) {
+  ROS_INFO_STREAM("Caught signal " << s);
+  exit(1);
 }
 
 int main(int argc, char **argv) {
+
+  bool training = false; // 0 = no training, 1 = training
+
+  // signal(SIGINT, my_handler);
+  // Capture ctrl + c event (Not working for some reason [BUG])
+
+  /*  Parse all input arguments   */
+  for (;;) {
+    switch (getopt(argc, argv, "t:")) {
+    case 't':
+      training = (bool)(atoi(optarg) > 0);
+      continue;
+
+    default:
+      /*   Add explain print?  */
+      break;
+
+    case -1:
+      break;
+    }
+    break;
+  }
+  ROS_INFO_STREAM("Training: " << training);
 
   ros::init(argc, argv, "get_action_node", ros::init_options::AnonymousName);
 
@@ -121,22 +220,86 @@ int main(int argc, char **argv) {
 
   ros::NodeHandle n;
   ros::Rate r(60);
+  PrologClient pl = PrologClient("/rosprolog", true);
+  if (pl.waitForServer()) {
+    ROS_INFO_STREAM("Started prolog...");
+  }
+
+  /*  Load ontology file if it exist  */
+  load_ontology(pl);
 
   // Clients
-  client_spawn = n.serviceClient<robotic_pusher::spawnObject>("spawn_object");
-  client_weight =
+  ros::ServiceClient client_spawn =
+      n.serviceClient<robotic_pusher::spawnObject>("spawn_object");
+  ros::ServiceClient client_weight =
       n.serviceClient<robotic_pusher::getWeightType>("weight_type_service");
+  ros::ServiceClient client_push =
+      n.serviceClient<robotic_pusher::getVelocity>("pusher_service");
 
-  srv.request.get_weight_class = true;
-  object.request.model_name = object_name;
+  ROS_INFO_STREAM("Connected to all services...");
 
-  // Service
-  ros::ServiceServer service =
-      n.advertiseService("get_velocity", &get_velocity);
+  robotic_pusher::spawnObject object_object;
+  robotic_pusher::getWeightType weight_object;
+  robotic_pusher::getVelocity velocity_object;
 
-  ROS_INFO_STREAM("Service is up...");
+  weight_object.request.get_weight_class = true;
+  object_object.request.model_name = object_name;
 
-  ros::spin();
+  /*  Open file to save ontology  */
+  ofstream ont_File;
+  ont_File.open(file_name);
+
+  while (ros::ok()) {
+
+    /*  Call the service to spawn a object  */
+    if (client_spawn.call(object_object)) {
+      ROS_INFO_STREAM("Response: " << object_object.response.reply);
+    } else {
+      ROS_ERROR_STREAM("Failed to spawn a random object");
+      return 1;
+    }
+
+    string object_weight_type;
+    string object_weight_color;
+    /*  Call the service to get the weight of the spawned object  */
+    if (client_weight.call(weight_object)) {
+      object_weight_type = weight_object.response.weight_type;
+      object_weight_color = weight_object.response.object_color;
+      ROS_INFO_STREAM("Response: " << object_weight_type << " "
+                                   << object_weight_color);
+    } else {
+      ROS_ERROR_STREAM("Failed to get the weight from the object");
+      return 1;
+    }
+
+    if (training) {
+      // Random between 0.f and 1.f
+      velocity_object.request.impact_velocity =
+          static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    } else {
+      velocity_object.request.impact_velocity =
+          get_action(object_weight_type, pl);
+    }
+
+    float traveled_distance;
+    if (client_push.call(velocity_object)) {
+      float x = velocity_object.response.position.x;
+      float y = velocity_object.response.position.y;
+      float z = velocity_object.response.position.z;
+      traveled_distance = sqrtf(x * x + y * y + z * z);
+      ROS_INFO_STREAM("Response: " << traveled_distance);
+    } else {
+      ROS_ERROR_STREAM("Failed to get the position from the object");
+      return 1;
+    }
+
+    if (training)
+      update_onotology(ont_File, traveled_distance,
+                       velocity_object.request.impact_velocity,
+                       object_weight_color, pl);
+  }
+  /*    Save ontology here   */
+  ont_File.close();
 
   return 0;
 }
